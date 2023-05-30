ed51a4fc235bcd3b25374407b46259ec985ce713
Chris <hakizachrist1@gmail.com>
2023-05-30 15:50:17 +0200

    Create routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  root 'users#index'
+  resources :users, only: [:index] do
+    resources :inventories, only: [:index, :new, :create, :show, :update, :destroy] 
+    resources :recipes, only: [:index, :show, :new, :create, :update, :destroy]
+  end 
+  
+  resources :public_recipes, only: [:index]
+
 end
+
+  
