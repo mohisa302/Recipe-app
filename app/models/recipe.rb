6fee9a03838b5bd402ca8dd2c182e7a69d7c5154
Chris <hakizachrist1@gmail.com>
2023-05-31 13:45:31 +0200

    Create recipes list

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :description, presence: true
+  validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
+  validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
+
+  def owner?(user)
+    self.user == user
+  end
 end
