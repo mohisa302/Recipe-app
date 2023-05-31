13fd14e1c9b30e5751fc3d339e0f7fdf170ad509
Ayo Moses <ayomoses111@gmail.com>
2023-05-31 10:22:39 +0100

    Implement users sign out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    if params[:id] == 'sign_out'
+      sign_out_and_redirect
+    else
+      find_user
+    end
+  end
+
+  private
+
+  def sign_out_and_redirect
+    sign_out current_user
+    redirect_to new_user_session_path
+  end
 end
