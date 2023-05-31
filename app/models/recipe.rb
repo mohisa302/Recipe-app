a9618ea2975bd45b2d1212b21c11ead9bfad7fc1
Chris <hakizachrist1@gmail.com>
2023-05-31 13:46:58 +0200

    fix linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  
+
   validates :name, presence: true, length: { maximum: 250 }
   validates :description, presence: true
   validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0.0 }
