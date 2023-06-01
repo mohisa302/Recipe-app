b201a17ca363dcceee9b6d9658bab1b221cc7849
Chris <hakizachrist1@gmail.com>
2023-06-01 15:43:37 +0200

    Update devise view

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_092839) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_072519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
   end
 
   create_table "inventories", force: :cascade do |t|
