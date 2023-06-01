00c3e828ede50fdda8b2f6eaada8b00decb5daf0
Chris <hakizachrist1@gmail.com>
2023-06-01 23:14:35 +0200

    Update recipes_controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
     @user = current_user
     @recipes = @user.recipes
