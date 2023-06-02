b33b48823dc5602c1d6a1457a4c65073d262ec1d
mohi <mohisadeghi302@gmail.com>
2023-06-02 14:48:07 +0330

    Fix login router

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  def after_sign_in_path_for(resource)
+    users_path
+  end
+
   protected
 
   def configure_permitted_parameters
