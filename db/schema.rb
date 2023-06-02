04fa99a52f7943223ac9cfd4d93814e0894099ad
Ayo Moses <ayomoses111@gmail.com>
2023-06-02 16:27:23 +0100

    Set up database

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_172610) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
