46858a6ac863a79ffc3782e351cedcbc57416f77
Ayo Moses <ayomoses111@gmail.com>
2023-06-03 07:34:16 +0100

    Fix linters

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,10 +1,5 @@
 class Inventory < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
   has_many :inventory_foods, dependent: :destroy
   has_many :foods, through: :inventory_foods, dependent: :destroy
-=======
-  has_many :inventory_foods
-  has_many :foods, through: :inventory_foods
->>>>>>> a7a0838efc69e3d1cb0918a91eb7ecd542f90ec1
 end
