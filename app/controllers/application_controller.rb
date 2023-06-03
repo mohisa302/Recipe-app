eb03c5d84840b0f5f126f8b75ce275930f26a03f
Mohi <MOHISADEGHI302@GMAIL.com>
2023-06-03 15:30:19 +0330

    Fix rubocop linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,6 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
-  def after_sign_in_path_for(_resource)
-    users_path
-  end
-
   def after_sign_in_path_for(_resource_or_scope)
     root_path
   end
--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'Food Index', type: :feature do
   before :each do
     user = User.create!(
       name: 'Tom',
-      email: 'tom@example.com',
+      email: 'ayomoses111@example.com',
       password: '123456',
       confirmed_at: Time.now
     )
