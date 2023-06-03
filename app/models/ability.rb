6bf851a0d221bb85d7954c29f231795d5240c711
Ayo Moses <ayomoses111@gmail.com>
2023-06-03 15:36:41 +0100

    Resolve all feedbacks from last PR

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,6 @@ class Ability
     if @user.id.present?
       can :read, :all
       can :create, Inventory
-      # can [:update, :destroy], Inventory, user_id: @user.id
       can :manage, Inventory, user_id: @user.id
     else
       can :read, Inventory
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
   devise_for :users
   root 'users#index'
   
   mount LetterOpenerWeb::Engine, at: '/letter_opener'
-
   resources :users, only: [:index, :show] do
     resources :recipes, only: [:index, :show, :new, :create, :update, :destroy]
   end 
   resources :inventories, only: [:index, :new, :create, :show, :update, :destroy] do
     resources :inventory_foods, only: [:create, :delete, :new]
   end
-
   resources :inventory_foods, only: [:create, :destroy, :new]
-
   resources :users, only: [:index, :show] do
     resources :foods, only: %i[index show new create destroy]
     resources :recipes, only: [:index, :show, :new, :create, :update, :destroy] do 
@@ -26,16 +21,11 @@ Rails.application.routes.draw do
       resources :recipe_foods, only: %i[new create edit update destroy]
     end 
     resources :inventories, only: [:index, :new, :create, :show, :update, :destroy] do
-      resources :inventory_foods, only: [:create, :delete, :new]
+      resources :inventory_foods, only: [:create, :destroy, :new]
     end
     resources :shopping_list, only: %i[index new create]
-
   end 
-
   resources :public_recipes, only: [:index, :show]
-  post '/inventory_foods', to: 'inventory_foods#create', as: 'create_inventory_food'
-  delete '/inventory_foods', to: 'inventory_foods#destroy', as: 'delete_inventory_food'
-  post '/inventories', to: 'inventories#create', as: 'create_inventory'
   get '/make_list', to: 'shopping_list#make_list', as: 'make_list'
   get '/recipes/:recipe_id/generate', to: 'shopping_list#generate_shopping_list', as: 'generate_shopping_list'
   get 'recipes/:recipe_id/new', to: 'shopping_list#new', as: 'new_user_recipe_shopping_list'
